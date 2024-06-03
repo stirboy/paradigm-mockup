472bd9107c00cd1d87e90b9c242265dcdc95e512
Alexei Stirbul <alshtirbul@gmail.com>
2024-06-03 19:58:05 +0200

    Fixing editor

--- fe/app/notes/_components/editor.tsx
+++ fe/app/notes/_components/editor.tsx
@@ -1,6 +1,24 @@
 "use client";
 
 import "@blocknote/core/fonts/inter.css";
+import {
+  BasicTextStyleButton,
+  BlockColorsItem,
+  BlockTypeSelect,
+  ColorStyleButton,
+  CreateLinkButton,
+  DragHandleMenu,
+  FileCaptionButton,
+  FileReplaceButton,
+  FormattingToolbar,
+  FormattingToolbarController,
+  NestBlockButton,
+  RemoveBlockItem,
+  SideMenu,
+  SideMenuController,
+  TextAlignButton,
+  UnnestBlockButton,
+} from "@blocknote/react";
 import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
@@ -8,6 +26,7 @@ import { useMemo } from "react";
 
 import "./editor.css";
 import { useTheme } from "next-themes";
+import { useSearch } from "@/app/notes/_hooks/use-search";
 
 interface NoteEditorProps {
   onChange: (content: string) => void;
@@ -21,6 +40,7 @@ const NoteEditor: React.FC<NoteEditorProps> = ({
   editable,
 }) => {
   const { resolvedTheme } = useTheme();
+  const { isOpen } = useSearch();
 
   const editor = useMemo(() => {
     return BlockNoteEditor.create({
@@ -37,6 +57,8 @@ const NoteEditor: React.FC<NoteEditorProps> = ({
   return (
     <div className="w-full h-full">
       <BlockNoteView
+        formattingToolbar={false}
+        sideMenu={false}
         editor={editor}
         editable={editable}
         theme={resolvedTheme === "dark" ? "dark" : "light"}
@@ -45,7 +67,77 @@ const NoteEditor: React.FC<NoteEditorProps> = ({
         }}
         ref={undefined}
         onSelectionChange={() => {}}
-      ></BlockNoteView>
+      >
+        {!isOpen && (
+          <>
+            <FormattingToolbarController
+              formattingToolbar={() => (
+                <FormattingToolbar>
+                  <BlockTypeSelect key={"blockTypeSelect"} />
+
+                  <FileCaptionButton key={"fileCaptionButton"} />
+                  <FileReplaceButton key={"replaceFileButton"} />
+
+                  <BasicTextStyleButton
+                    basicTextStyle={"bold"}
+                    key={"boldStyleButton"}
+                  />
+                  <BasicTextStyleButton
+                    basicTextStyle={"italic"}
+                    key={"italicStyleButton"}
+                  />
+                  <BasicTextStyleButton
+                    basicTextStyle={"underline"}
+                    key={"underlineStyleButton"}
+                  />
+                  <BasicTextStyleButton
+                    basicTextStyle={"strike"}
+                    key={"strikeStyleButton"}
+                  />
+                  {/* Extra button to toggle code styles */}
+                  <BasicTextStyleButton
+                    key={"codeStyleButton"}
+                    basicTextStyle={"code"}
+                  />
+
+                  <TextAlignButton
+                    textAlignment={"left"}
+                    key={"textAlignLeftButton"}
+                  />
+                  <TextAlignButton
+                    textAlignment={"center"}
+                    key={"textAlignCenterButton"}
+                  />
+                  <TextAlignButton
+                    textAlignment={"right"}
+                    key={"textAlignRightButton"}
+                  />
+
+                  <ColorStyleButton key={"colorStyleButton"} />
+
+                  <NestBlockButton key={"nestBlockButton"} />
+                  <UnnestBlockButton key={"unnestBlockButton"} />
+
+                  <CreateLinkButton key={"createLinkButton"} />
+                </FormattingToolbar>
+              )}
+            />
+            <SideMenuController
+              sideMenu={(props) => (
+                <SideMenu
+                  {...props}
+                  dragHandleMenu={(props) => (
+                    <DragHandleMenu {...props}>
+                      <RemoveBlockItem {...props}>Delete</RemoveBlockItem>
+                      <BlockColorsItem {...props}>Colors</BlockColorsItem>
+                    </DragHandleMenu>
+                  )}
+                />
+              )}
+            />
+          </>
+        )}
+      </BlockNoteView>
     </div>
   );
 };
